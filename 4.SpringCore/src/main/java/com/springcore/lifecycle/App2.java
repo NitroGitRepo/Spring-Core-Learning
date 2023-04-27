bf225e0ea032b0aa0253b04b280bbd5f42d65a26
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2023-04-28 01:32:31 +0530

    Applying Lifecycle Method Using XML Technique

---
+++ 4.SpringCore/src/main/java/com/springcore/lifecycle/App2.java
@@ -0,0 +1,23 @@
+package com.springcore.lifecycle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/lifecycle/config2.xml");
+        Samosa s1 = (Samosa)context.getBean("bean1");
+        
+        System.out.println(s1);
+        
+        //registering shutdown hook->call destroy method which is given in destroy-method attribute 
+        context.registerShutdownHook(); 
+        
+        
+	}
+
+}
---
+++ 4.SpringCore/src/main/java/com/springcore/lifecycle/Samosa.java
@@ -0,0 +1,37 @@
+package com.springcore.lifecycle;
+
+public class Samosa {
+
+	 private double price;
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		System.out.println("Setting Price");
+		this.price = price;
+	}
+
+	public Samosa() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	//init() method
+	public void init1() {
+		System.out.println("Inside Init Method");
+	}
+	
+	public void destroy1() {
+		System.out.println("Inside Destroy Method");
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Samosa [price=" + price + "]";
+	}
+	 
+	 
+}
