53481f95c922de3944984240570bb0e663a6fb82
NitroGitRepo <rajneesh.yadav.aws@gmail.com>
2023-05-16 01:28:10 +0530

    Stereotype Annotation @Component @Value @Component("bean name")

---
+++ 5.SpringCore/src/main/java/com/springcore/componentannotation/App4.java
@@ -0,0 +1,19 @@
+package com.springcore.componentannotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/componentannotation/config4.xml");
+		
+         Student student1 = (Student)context.getBean("stu");
+ 
+         System.out.println(student1);
+		
+	}
+
+}
